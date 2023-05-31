f4309ca360a357742b6546da6ac7e253892a9842
Jeremy Libretti-River <jeremylibretti@gmail.com>
2023-05-31 13:53:56 -0500

    somethin somethin
    Co-authored-by: Brevan Mitchell <brevanm@users.noreply.github.com>

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+type Ingredient = { 
+  amount: number;
+  measurment: string;
+  ingredient: string;
+};
+
+type Recipe = {
+  title: string;
+  imgUrl: string;
+  tags: string[];
+  ingredients: Ingredient[];
+  instructions: string;
+};
+
 export default {
   getProfile: (token: string) => {
     const endpoint = `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`;
